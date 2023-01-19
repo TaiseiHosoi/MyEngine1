cb073aeddb1b6a1dd2ed3648cda2edf5a70ba27c
LAPTOP-RL7QBGDD\taise <taiseigaming@gmail.com>
2023-01-19 11:50:51 +0900

    taihi

--- GameScene.cpp
+++ GameScene.cpp
@@ -27,7 +27,7 @@ void GameScene::Initialize(DirectXCommon* dxcomon)
 	sprite2->Initialize(spriteCommon_, 2);
 
 
-	sprite1->SetSize({ 200,200 });
+	sprite1->SetSize({ 1280,720 });
 	sprite2->SetSize({ 200,200 });
 	sprite1->SetPozition({ 100,100 });
 	sprite2->SetPozition({ 100,200 });
@@ -61,4 +61,165 @@ void GameScene::Draw()
 
 	Object3d::PostDraw();
 
+}
+
+
+void GameScene::AddBullet(std::unique_ptr<Bullet>& Bullet)
+{
+	bullets_.push_back(std::move(Bullet));
+}
+
+void GameScene::GenerBullet(Vector3 BulletPos, int ID)
+{
+	//生成
+	std::unique_ptr<Bullet> newBullet = std::make_unique<Bullet>();
+	//敵キャラの初期化
+	float kBulSpeed = 0.4f;
+	if (gameLevel_ > 0) {
+		kBulSpeed += gameLevel_ * 0.1f + 1.0f;
+	}
+
+
+	newBullet->Initialize(model, testTexture_, BulletPos, kBulSpeed);
+
+	newBullet->SetID(ID);
+
+	//リストに登録する
+	bullets_.push_back(std::move(newBullet));
+
+}
+
+void GameScene::LoadBulletPopData()
+{
+
+	//ファイルを開く
+	std::ifstream file;
+	file.open("Resources/enemyPop2.csv");
+
+	assert(file.is_open());
+
+	//ファイルの内容を文字列ストリームにコピー
+	bulletPopCommands_ << file.rdbuf();
+
+	//ファイルを閉じる
+	file.close();
+}
+
+void GameScene::UpdateBulletPopCommands()
+{
+	//待機処理
+	if (isStand_) {
+		standTime_--;
+		if (standTime_ <= 0) {
+			//待機完了
+			isStand_ = false;
+		}
+		return;
+	}
+	// 1行分の文字列を入れる変数
+	std::string line;
+
+	//コマンド実行ループ
+	while (getline(bulletPopCommands_, line)) {
+		// 1行分の文字数をストリームに変換して解折しやすくなる
+		std::istringstream line_stream(line);
+
+		std::string word;
+		//,区切りで行の先頭文字を取得
+		getline(line_stream, word, ',');
+
+		//"//"から始まる行はコメント
+		if (word.find("//") == 0) {
+			//コメント行を飛ばす
+			continue;
+		}
+		// POPコマンド
+		if (word.find("POP") == 0) {
+
+			//レーン
+			std::getline(line_stream, word, ',');
+			int lane = static_cast<int>(std::atof(word.c_str()));
+
+			// ID
+			std::getline(line_stream, word, ',');
+			int ID = static_cast<int>(std::atof(word.c_str()));
+
+			float depth = 40.0f;	//奥行
+			float xDifference = 10.0f;	//左右差
+			if (lane == 1) {
+				GenerBullet(Vector3(-xDifference, 0, depth), ID);
+			}
+			else if (lane == 2) {
+				GenerBullet(Vector3(0, 0, depth), ID);
+			}
+			else if (lane == 3) {
+				GenerBullet(Vector3(xDifference, 0, depth), ID);
+			}
+			else {
+				GenerBullet(Vector3(0, 3.0f, depth), ID);
+			}
+		}
+		// WAITコマンド
+		else if (word.find("WAIT") == 0) {
+			std::getline(line_stream, word, ',');
+
+			//待ち時間
+			int32_t waitTime = std::atoi(word.c_str());
+
+			//待機開始
+			isStand_ = true;
+			int maxTimeDiv = 10;
+			if (gameLevel_ <= 0) {
+				standTime_ = waitTime * (maxTimeDiv - gameLevel_) / maxTimeDiv;
+			}
+			else {
+
+				standTime_ = waitTime * (maxTimeDiv - gameLevel_) / maxTimeDiv;
+			}
+
+			//抜ける
+			break;
+		}
+	}
+}
+
+void GameScene::BulletReset()
+{
+	bulletPopCommands_.str("");
+	bulletPopCommands_.clear(std::stringstream::goodbit);
+	LoadBulletPopData();
+}
+
+
+void GameScene::CheckAllCollisions() {
+
+	//判定対象AとBの座標
+	Vector3 posA, posB;
+
+
+
+#pragma region 自弾と敵キャラの当たり判定
+	//敵キャラの座標
+	for (std::unique_ptr<Bullet>& bullet_ : bullets_) {
+		posA = bullet_->GetWorldPosition();
+
+
+		//自弾の座標
+		posB = goal_->GetWorldPosition();
+
+		float x = posB.x - posA.x;
+		float y = posB.y - posA.y;
+		float z = posB.z - posA.z;
+
+		float cd = sqrt(x * x + y * y + z * z);
+
+		if (cd <= 4.0f) {
+			//敵キャラの衝突時コールバックを呼び出す
+			bullet_->OnCollision();
+
+			//衝突時コールバックを呼び出す
+			//goal_->OnCollision();
+		}
+
+	}
 }
--- GameScene.h
+++ GameScene.h
@@ -6,6 +6,16 @@
 #include "Audio.h"
 #include "Mesh.h"
 
+#include "sstream"
+#include "memory"
+#include "list"
+
+#include"Goal.h"
+#include"Field.h"
+#include"Enemy.h"
+#include"Ease.h"
+
+
 class GameScene {
 public:
 	/// <summary>
@@ -33,6 +43,32 @@ public:
 	/// </summary>
 	void Draw();
 
+	//弾リストを取得
+	std::list<std::unique_ptr<Bullet>> bullets_;
+	const std::list<std::unique_ptr<Bullet>>& GetBullets() { return bullets_; }
+
+	//弾を足す
+	void AddBullet(std::unique_ptr<Bullet>& Bullet);
+	void GenerBullet(Vector3 BulletPos, int ID);
+
+	/// <summary>
+	/// 敵発生データ読み込み
+	/// </summary>
+	void LoadBulletPopData();
+	void BulletReset();
+	void UpdateBulletPopCommands();
+
+	//当たり判定
+	void CheckAllCollisions();
+
+	int gameLevel_ = 0;
+	int levelMax_ = 8;
+	//レーン
+	Field field_[3];
+
+	//ゴール用
+	Goal* goal_ = nullptr;
+
 private:
 	DirectXCommon* dxCommon_ = nullptr;
 	
@@ -44,5 +80,22 @@ private:
 private://リソース
 	Sprite* sprite1 = nullptr;
 	Sprite* sprite2 = nullptr;
+
+	//csv
+	std::stringstream bulletPopCommands_;
+
+	//テクスチャ
+	uint32_t testTexture_ = 0;
+
+	//レーンデバッグ用のテクスチャ
+	uint32_t laneTexture_[3];
+
+	//待機中フラグ
+	bool isStand_ = false;
+	//待機タイマー
+	int standTime_ = 0;
+
+	int gameTimer_ = 0;
+
 	
 };
