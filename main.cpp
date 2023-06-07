d31b3202bf17c6d9cc254fb599a14a86172dda7f
TaiseiHosoi <taiseigaming@gmail.com>
2023-06-07 17:38:09 +0900

    engine分け

--- main.cpp
+++ main.cpp
@@ -1,18 +1,8 @@
-﻿#include"WinApp.h"
-#include"DirectXCommon.h"
+﻿#include"SummarizeEngine/SummarizeEngine.h"
 #include"Error.h"
-#include"Mesh.h"
-#include"input.h"
-#include"Object3d.h"
-#include"Sprite.h"
-#include"Audio.h"
-#include"GameScene.h"
-#include"ImGuiManager.h"
-#include"fbxsdk.h"
 
 
 
-const float PI = 3.14f;
 
 //using namespace Microsoft::WRL;
 template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
@@ -31,109 +21,30 @@ template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
 //}
 
 
-
 //Windowsアプリでのエントリーポイント（main関数）
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 //コンソールへの文字出力
-{
+{	
+	
 
+	std::unique_ptr <SummarizeEngine> engine_;
+	engine_ = std::make_unique<SummarizeEngine>();
 
-	//OutputDebugStringA("Hello,DrectX!!/n");
-
-	//ポインタ宣言
-	WinApp* winApp_ = nullptr;
-	winApp_ = WinApp::GetInstance();
-
-
-	DirectXCommon* dxCommon_ = nullptr;
-	dxCommon_ = new DirectXCommon;
-
-	Audio* audio_ = nullptr;
-	audio_ = Audio::GetInstance();
-
-
-	//winApp初期化
-	winApp_->Initialize();
+	engine_->Initialize();
 
 	MSG msg = {};
-	//DirectX初期化処理　ここから
-	dxCommon_->Initialize(winApp_);
-
-	audio_->Initialize();
-
-	Input* input_ = Input::GetInstance();
-	input_->Initialize(winApp_);
-
-	// FBX関連静的初期化
-	FbxLoader::GetInstance()->Initialize(dxCommon_->GetDevice());
-
-	// 3Dオブジェクト静的初期化
-	Object3d::StaticInitialize(dxCommon_->GetDevice(), WinApp::window_width, WinApp::window_height);
-
-	//パーティクル静的初期化
-	ParticleManager::StaticInitialize(dxCommon_->GetDevice(),dxCommon_->GetCommandList());
-
-
-	GameScene* gameScene_ = nullptr;
-	gameScene_ = new GameScene();
-	gameScene_->Initialize(dxCommon_);
-
-	ImGuiManager* imGuiManager_ = nullptr;
-	imGuiManager_ = new ImGuiManager;
-	imGuiManager_->Initialize(winApp_,dxCommon_);
-
 	
 
 
-	while (true) {
-		if (winApp_->ProcessMessage()) {
-			break;
-		}
-
-		//更新
-		input_->Update();
-		imGuiManager_->Begin();
-		gameScene_->Update();
-
-		if (input_->TriggerKey(DIK_ESCAPE))
-		{
-			break;
-		}
+	while (engine_->isRunningGame) {
+		engine_->Update();
 		
 		//描画
-		dxCommon_->PreDraw();
-
-
-		gameScene_->Draw();
-
-		imGuiManager_->End();
-
-
-		imGuiManager_->Draw();
-
-		dxCommon_->PostDraw();
+		engine_->Draw();
 
 	}
 
-	winApp_->Finalize();
-	imGuiManager_->Finalize();
-	audio_->Finalize();
-
-	//ID3D12DebugDevice* debugInterface;
-
-	//if (SUCCEEDED(dxCommon_->GetDevice()->QueryInterface(&debugInterface))) {
-	//	debugInterface->ReportLiveDeviceObjects(D3D12_RLDO_DETAIL | D3D12_RLDO_IGNORE_INTERNAL);
-	//	debugInterface->Release();
-	//}
-	//元データ解放
-	//delete[] imageData;
-	//入力開放
-
-	delete gameScene_;
-	delete imGuiManager_;
-	delete dxCommon_;
-	delete winApp_;
-	
+	engine_->Finalize();
 
 	return 0;
 }
