763d16026ffee588758c9a378238060bebc205a4
suga <k021g1264@g.neec.ac.jp>
2023-07-03 06:48:06 +0900

    設定変更とutf-8に変更

---
+++ PlayerReticle.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "Object3d.h"
+class PlayerReticle
+{
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw(ID3D12GraphicsCommandList* cmdList);
+
+private:
+	/// <summary>
+	/// オブジェクト
+	/// </summary>
+
+	//近距離レティクル
+	std::unique_ptr<Object3d> nierReticleO_;
+	std::unique_ptr<Mesh> nierReticleM_;
+
+	//遠距離レティクル
+	std::unique_ptr<Object3d> farReticleO_;
+	std::unique_ptr<Mesh> farReticleM_;
+
+
+
+
+};
+
