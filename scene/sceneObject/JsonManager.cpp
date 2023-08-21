431ab7e854ac87eb34563bf7d1cf7139e674c513
suga <k021g1264@g.neec.ac.jp>
2023-08-21 12:31:37 +0900

    a

--- scene/sceneObject/JsonManager.cpp
+++ scene/sceneObject/JsonManager.cpp
@@ -125,7 +125,6 @@ void JsonManager::UpdateAllObjects()
 		if (moaiState[i].isAlive_ == false) {	//死去
 			moaiObjs.erase(std::cbegin(moaiObjs) + i);
 			moaiSpCollider.erase(std::cbegin(moaiSpCollider) + i);
-			moaiSpCollider[i]->SetAttribute(COLLISION_ATTR_ALLIES);
 			moaiState.erase(std::cbegin(moaiState) + i);
 		}
 		moaiSpCollider[i]->Update();
